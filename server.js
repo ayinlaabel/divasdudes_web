45a98e7eb2528e22af62aba4d94b254d95fe59c8
ayinlaabel <kraneabel@gmail.com>
2022-02-10 23:35:49 +0100

    Initail Commit

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/divasdudes'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/divasdudes/index.html'));});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/divasdudes/index.html'));
+});
 app.listen(process.env.PORT || 8080);
